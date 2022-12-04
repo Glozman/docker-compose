07d63f34ca6676be1eb4eccd5d5b893bdd07adae
stephan <stephan@pop-os.localdomain>
2022-12-04 11:59:08 +0200

    final

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /usr/app
 
 COPY package.json .
 
+
 RUN npm install --quiet
 
 COPY . .
+ENTRYPOINT ["node", "/usr/app/index.js"]
+
--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 const axios = require("axios")
-app.get('/', async (req, res) => {
+
+app.get('/', (req, res) => {
   try {
-    const response = await axios.get('http://node2:4000/say')
-    res.send(response.data)
+    // const response = await axios.get('http://node2:4000/say')
+    console.log('get')
+    res.send('get request 1')
   }
   catch (err) {
     console.log(err);
   }
+})
+
+app.post('/', (req, res) => {
+  const body = req.body
+  console.log('post 1')
+  console.log(body)
+  res.json({body,msg:'post 1'})
 })
 
 app.listen(port, () => {
